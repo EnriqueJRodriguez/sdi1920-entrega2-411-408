8be5674f205698df82077cf772c90e634cf487a5
EnriqueJRodriguez <enriquejrodriguezmartin@gmail.com>
2020-04-17 17:04:17 +0200

    Base de datos operativa, falta control de enrutados y acceder al usuario en sesion desde cualquier punto de la aplicacion

--- app.js
+++ app.js
@@ -2,6 +2,19 @@
 var express = require('express');
 var app = express();
 
+let mongo = require('mongodb');
+let gestorBD = require("./modules/gestorBD.js");
+gestorBD.init(app,mongo);
+
+
+let crypto = require('crypto');
+let expressSession = require('express-session');
+app.use(expressSession({
+    secret: 'abcdefg',
+    resave: true,
+    saveUninitialized: true
+}));
+
 let swig = require('swig');
 let bodyParser = require('body-parser');
 app.use(bodyParser.json());
@@ -12,11 +25,34 @@ app.use(express.static('public/img'));
 
 // Variables
 app.set('port', 8081);
+app.set('db','mongodb://admin:robertocornellananoexistesonlospadres@sdi1920-entrega2-411-408-shard-00-00-axx04.mongodb.net:27017,sdi1920-entrega2-411-408-shard-00-01-axx04.mongodb.net:27017,sdi1920-entrega2-411-408-shard-00-02-axx04.mongodb.net:27017/test?ssl=true&replicaSet=sdi1920-entrega2-411-408-shard-0&authSource=admin&retryWrites=true&w=majority');
+app.set('clave','abcdefg');
+app.set('crypto',crypto);
 
 //Rutas/controladores por lógica
-require("./routes/rusuarios.js")(app,swig); // (app, param1, param2, etc.)
+require("./routes/rusuarios.js")(app,swig, gestorBD); // (app, param1, param2, etc.)
+
+// routerUsuarioSession
+let routerUsuarioSession = express.Router();
+routerUsuarioSession.use(function(req, res, next) {
+    console.log("routerUsuarioSession");
+    if ( req.session.usuario ) {
+        // dejamos correr la petición
+        next();
+    } else {
+        console.log("va a : "+req.session.destino)
+        res.redirect("/identificarse");
+    }
+});
+
+//Aplicar routerUsuarioSession
+app.use("/desconectarse",routerUsuarioSession);
+app.use("/home",routerUsuarioSession);
+
+app.use(express.static('public'));
+
 
 // lanzar el servidor
 app.listen(app.get('port'), function() {
-    console.log("Servidor activo en el puerto 8081  ");
+    console.log("Servidor activo en el puerto 8081");
 })
--- routes/rusuarios.js
+++ routes/rusuarios.js
@@ -1,5 +1,67 @@
-module.exports = function(app, swig) {
-    app.get("/usuarios", function(req, res) {
-        res.send("ver usuarios");
+module.exports = function(app, swig, gestorBD) {
+    app.get("/", function(req, res) {
+        let respuesta = swig.renderFile('views/index.html',{});
+        res.send(respuesta);
     });
+
+    app.get("/home", function(req, res) {
+        let respuesta = swig.renderFile('views/home.html',{});
+        res.send(respuesta);
+    });
+
+    app.get("/registrarse", function(req, res) {
+        let respuesta = swig.renderFile('views/bregistro.html', {});
+        res.send(respuesta);
+    });
+
+    app.get("/identificarse", function(req, res) {
+        let respuesta = swig.renderFile('views/bidentificacion.html', {});
+        res.send(respuesta);
+    });
+
+    app.post('/usuario', function(req, res) {
+        if(req.body.password != req.body.repassword){
+            // TEMPORAL MAS TARDE LANZAR ERROR
+            res.redirect("/registrarse");
+            return;
+        }
+        let seguro = app.get("crypto").createHmac('sha256', app.get('clave'))
+            .update(req.body.password).digest('hex');
+        let usuario = {
+            email : req.body.email,
+            name : req.body.name,
+            surname : req.body.surname,
+            password : seguro
+        };
+        gestorBD.insertarUsuario(usuario, function(id) {
+            if (id == null){
+                res.redirect("/usuario");
+            } else {
+                res.redirect("/");
+            }
+        });
+    });
+
+    app.post("/identificarse", function(req, res) {
+        let seguro = app.get("crypto").createHmac('sha256', app.get('clave'))
+            .update(req.body.password).digest('hex');
+        let criterio = {
+            email : req.body.email,
+            password : seguro
+        }
+        gestorBD.obtenerUsuarios(criterio, function(usuarios) {
+            if (usuarios == null || usuarios.length == 0) {
+                req.session.usuario = null;
+                res.redirect("/identificarse");
+            } else {
+                req.session.usuario = usuarios[0].nombre;
+                res.redirect("/home");
+            }
+        });
+    });
+
+    app.get('/desconectarse', function (req, res) {
+        req.session.usuario = null;
+        res.redirect("/");
+    })
 };

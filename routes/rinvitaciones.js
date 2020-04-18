0183f1e57e5478315e543264b72f711568e72350
EnriqueJRodriguez <enriquejrodriguezmartin@gmail.com>
2020-04-18 22:05:54 +0200

    W7 comprobado y operativo

--- routes/rinvitaciones.js
+++ routes/rinvitaciones.js
@@ -33,4 +33,72 @@ module.exports = function(app, swig, gestorBD) {
             }
         })
     });
+    app.get("/invitation/list", function (req, res) {
+        let criterio;
+        if( req.query.busqueda != null ){
+            criterio = {
+                '_id': {$not: {$eq: gestorBD.mongo.ObjectID(req.session.usuario._id)}},
+                $or:[
+                    {'name': new RegExp(req.query.busqueda + "+", 'i')},
+                    {'surname': new RegExp(req.query.busqueda + "+", 'i')},
+                    {'email': new RegExp(req.query.busqueda + "+", 'i')},
+                ],
+                'rol': {$not: {$eq: "ADMINISTRADOR"}}
+            };
+        }else {
+            criterio = {
+                '_id': {$not: {$eq: gestorBD.mongo.ObjectID(req.session.usuario._id)}},
+                'rol': {$not: {$eq: "ADMINISTRADOR"}}
+            };
+        }
+        let pg = parseInt(req.query.pg); // Es String !!!
+        if ( req.query.pg == null){ // Puede no venir el param
+            pg = 1;
+        }
+        gestorBD.obtenerUsuariosPg(criterio, pg , function(usuarios, total ) {
+            if (usuarios == null) {
+                res.redirect("/home"+ "?mensaje=Ha ocurrido un problema al mostar sus invitaciones de amistad"+
+                    "&tipoMensaje=alert-danger ");
+            } else {
+                let ultimaPg = total/5;
+                if (total % 5 > 0 ){ // Sobran decimales
+                    ultimaPg = ultimaPg+1;
+                }
+                let paginas = []; // paginas mostrar
+                for(let i = pg-2 ; i <= pg+2 ; i++){
+                    if ( i > 0 && i <= ultimaPg){
+                        paginas.push(i);
+                    }
+                }
+                usuarios = calcularInvitacionesUsuario(usuarios,req.session.usuario);
+                if(usuarios != null) {
+                    let respuesta = swig.renderFile('views/binvitationslist.html', {
+                        usuario: req.session.usuario,
+                        usuarios: usuarios,
+                        paginas: paginas,
+                        actual: pg
+                    });
+                    res.send(respuesta);
+                } else{
+                    res.redirect("/home"+ "?mensaje=Ha ocurrido un problema al mostar sus invitaciones de amistad"+
+                        "&tipoMensaje=alert-danger ");
+                }
+            }
+        });
+    });
+
+    function calcularInvitacionesUsuario(usuarios,usuario){
+        if(usuarios == null || usuario == null){
+            return null;
+        }
+        let userInvites = [];
+        let pointer = 0;
+        for(i=0; i<usuarios.length;i++){
+            if(usuario.invites.includes(usuarios[i]._id.toString())){
+                userInvites[pointer] = usuarios[i];
+                pointer++;
+            }
+        }
+        return userInvites;
+    }
 }

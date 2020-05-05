0282833fa8e7abe898e4d09586cbd0b068c2a751
fonsecadh <UO258318@uniovi.es>
2020-05-05 19:49:35 +0200

    Cliente - Cliente y widgets.

---
+++ public/js/api/cliente.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    var token;
+    var URLbase = "https://localhost:8081/api";
+    $("#contenedor-principal").load("widget-login.html");
+
+    if (Cookies.get('token') != null) { 
+        token = Cookies.get('token');
+        var url = new URL(window.location.href);
+        var w = url.searchParams.get("w");
+        if (w == "login") { 
+            $("#contenedor-principal").load("widget-login.html");
+        }
+        if (w == "amigos") { 
+            $("#contenedor-principal").load("widget-amigos.html");
+        }
+    }
+
+    function widgetCanciones() { 
+        $("#contenedor-principal").load("widget-amigos.html");
+    }
+
+    $("#div-errores").hide();
+});
+
---
+++ public/js/api/widget-login.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    window.history.pushState("", "", "/cliente.html?w=login");
+    $("#boton-login").click(function() {
+        $.ajax({
+            url : URLbase + "/autenticar",
+            type : "POST",
+            data : {
+                email : $("#email").val(),
+                password : $("#password").val()
+            },
+            dataType : 'json',
+            success : function(respuesta) {
+                console.log(respuesta.token);
+                token = respuesta.token;
+                Cookies.set('token', respuesta.token);
+                $("#contenedor-principal").load("widget-amigos.html");
+            },
+            error : function(error) {
+                Cookies.remove('token');
+                $("#widget-login").prepend("<div class='alert alert-danger'>Usuario no encontrado</div>");
+            }
+        });
+    });
+});
